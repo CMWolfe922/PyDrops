a12b6a7bab46cc9665964074838d235c56ece867
CMWolfe <cmwolfe.dev@gmail.com>
2022-10-13 15:44:13 -0500

    Fixed the views file

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
+
+from inspect import _SourceObjectType
 from queue import Empty
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+
 
 def post_list(request):
     post_list = Post.published.all()
@@ -28,7 +31,6 @@ def post_list(request):
     context = {
         'posts': posts,
         }
-
     return render(request,
                  'blog/post/list.html',
                  context)
@@ -41,11 +43,10 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    # Get a list of active comments for this post
+    # Get a list of active comments for this post:
     comments = post.comments.filter(active=True)
     # Now get the form for users to comment
     form = CommentForm()
-
 
     context = {
         'post': post,
@@ -53,7 +54,8 @@ def post_detail(request, year, month, day, post):
         'form': form,
     }
 
-    return render(request,'blog/post/detail.html', context)
+    return render(request, 'blog/post/detail.html', context)
+
 
 # Create a clasd bassed view
 class PostListView(ListView):
@@ -65,17 +67,18 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+
+# Create a form view 
 def post_share(request, post_id):
-    # Retrieve post by id
+    # retrieve post by ID
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
-    # Create a sent equals false variable
-    sent = False
     if request.method == 'POST':
-        # Form was submitted
+        # form was submitted 
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
+            # ... send email
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
@@ -83,14 +86,15 @@ def post_share(request, post_id):
             sent = True
     else:
         form = EmailPostForm()
-
+    
     context = {
         'post': post,
         'form': form,
         'sent': sent,
     }
 
-    return render(request, 'blog/post/share.html', context)
+    return render(request,'blog/post/share.html', context)
+
 
 @require_POST
 def post_comment(request, post_id):
