5d8d998170c30ce1deb3152bc7e674404e1a5ad7
Charles Wolfe <cmwolfe.dev@gmail.com>
2022-10-20 17:44:25 -0500

    Made updates to multiple files

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 
-from inspect import _SourceObjectType
-from queue import Empty
+
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
